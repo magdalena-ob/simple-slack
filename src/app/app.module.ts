ed819aaab46841d6ae7df965be8f013def5ba9ce
Mads Kristian Hansen <mads_hansen@hotmail.de>
2022-02-05 17:12:49 +0100

    basic component structure added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StartComponent } from './start/start.component';
+import { LoginComponent } from './start/login/login.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { NavigationComponent } from './main-page/navigation/navigation.component';
+import { DialogComponent } from './main-page/dialog/dialog.component';
+import { ChannelComponent } from './main-page/dialog/channel/channel.component';
+import { DirectMessageComponent } from './main-page/dialog/direct-message/direct-message.component';
+import { ThreadComponent } from './main-page/dialog/thread/thread.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    LoginComponent,
+    MainPageComponent,
+    NavigationComponent,
+    DialogComponent,
+    ChannelComponent,
+    DirectMessageComponent,
+    ThreadComponent
   ],
   imports: [
     BrowserModule,
