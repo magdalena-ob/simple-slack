df265d2b8fefeb2368ae62af1aea5c339880693e
magdalena-o <magdalena.obermayr@gmx.at>
2022-02-13 19:26:42 +0100

    click function for open dialog add-channel

--- src/app/search-channel/search-channel.component.ts
+++ src/app/search-channel/search-channel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddChannelComponent } from '../dialog-add-channel/dialog-add-channel.component';
 
 @Component({
   selector: 'app-search-channel',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchChannelComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  openAddChannel() {
+    this.dialog.open(DialogAddChannelComponent);
   }
 
 }
