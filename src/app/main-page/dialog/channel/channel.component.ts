89ee2e98d7bee659fe6b97bf0ff691aae4c3d73b
magdalena-o <magdalena.obermayr@gmx.at>
2022-02-14 14:44:12 +0100

    show current channel with ID in channel.component

--- src/app/main-page/dialog/channel/channel.component.ts
+++ src/app/main-page/dialog/channel/channel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { ActivatedRoute } from '@angular/router';
 import { Channel } from 'src/models/channel.class';
 
@@ -10,15 +11,27 @@ import { Channel } from 'src/models/channel.class';
 export class ChannelComponent implements OnInit {
 
   channelId: any = '';
-  channel = new Channel();
+  channel: Channel = new Channel();
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private firestore: AngularFirestore) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( paramMap => {
       this.channelId = paramMap.get('id');
       console.log('got channel id ', this.channelId);
+      this.getChannel();
     })
+  }
+
+  getChannel() {
+    this.firestore
+      .collection('channels')
+      .doc(this.channelId)
+      .valueChanges()
+      .subscribe((channel: any) => {
+        this.channel = new Channel(channel);
+        console.log('retrieved channel ', this.channel);
+      })
   }
 
 }
