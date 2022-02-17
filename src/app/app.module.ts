cf98938b3f1d831c11c864b9de9ca3321f1ecb99
magdalena-o <magdalena.obermayr@gmx.at>
2022-02-17 14:05:51 +0100

    deleted unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StartComponent } from './start/start.component';
 import { LoginComponent } from './start/login/login.component';
 import { MainPageComponent } from './main-page/main-page.component';
-import { NavigationComponent } from './main-page/navigation/navigation.component';
 import { DialogComponent } from './main-page/dialog/dialog.component';
 import { ChannelComponent } from './main-page/dialog/channel/channel.component';
 import { DirectMessageComponent } from './main-page/dialog/direct-message/direct-message.component';
@@ -61,7 +60,6 @@ import { MatExpansionModule } from '@angular/material/expansion';
     StartComponent,
     LoginComponent,
     MainPageComponent,
-    NavigationComponent,
     DialogComponent,
     ChannelComponent,
     DirectMessageComponent,
