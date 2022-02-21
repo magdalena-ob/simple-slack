459e0b68b9cbafd49e358ea4cda11a5ab04173b7
magdalena-o <magdalena.obermayr@gmx.at>
2022-02-21 14:06:31 +0100

    changed some properties from channel to user class

--- src/models/channel.class.ts
+++ src/models/channel.class.ts
@@ -1,22 +1,16 @@
 export class Channel {
     category: string;
     description: string;
-    timeSent: number;
-    image: any;
 
     constructor(obj ? : any){
         this.category = obj ? obj.category : '';        
         this.description = obj ? obj.description : '';
-        this.timeSent = obj ? obj.timeSent : '';
-        this.image = obj ? obj.image : '';
     }
 
     public toJSON() {
         return {
             category: this.category,
             description: this.description,
-            timeSent: this.timeSent,
-            image: this.image
         }    
     }
 }
--- src/models/message.class.ts
+++ src/models/message.class.ts
@@ -1,14 +1,20 @@
 export class Message {
     textMessage: string;
+    timeSent: number;
+    image: any;
 
     constructor(obj?: any) {
             this.textMessage = obj ? obj.textMessage : '';
+            this.timeSent = obj ? obj.timeSent : '';
+            this.image = obj ? obj.image : '';
            
     }
 
     public toJSON() {
         return {
-            textMessage: this.textMessage
+            textMessage: this.textMessage,
+            timeSent: this.timeSent,
+            image: this.image
             
         }
     }
