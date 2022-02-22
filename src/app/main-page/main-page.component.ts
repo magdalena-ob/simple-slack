de3b7f9f8ecdcebad9a804f6e266c2330b39868f
magdalena-o <magdalena.obermayr@gmx.at>
2022-02-22 10:40:45 +0100

    logout at main-page works

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddChannelComponent } from '../dialog-add-channel/dialog-add-channel.component';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-main-page',
@@ -9,13 +12,17 @@ import { DialogAddChannelComponent } from '../dialog-add-channel/dialog-add-chan
 })
 export class MainPageComponent implements OnInit {
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, public afAuth: AngularFireAuth, private firestore: AngularFirestore) { }
 
   ngOnInit(): void {
   }
   
   openAddChannel() {
     this.dialog.open(DialogAddChannelComponent);
+  }
+
+  logout():void {
+    this.afAuth.signOut();
   }
 
 }
