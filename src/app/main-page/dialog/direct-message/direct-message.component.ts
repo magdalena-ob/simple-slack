df402c71fb854c9884088f8598bee42076ab66ea
magdalena-ob <you@example.com>
2022-08-04 12:06:03 +0200

    show current chat name in direct-message

--- src/app/main-page/dialog/direct-message/direct-message.component.ts
+++ src/app/main-page/dialog/direct-message/direct-message.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-direct-message',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectMessageComponent implements OnInit {
 
-  constructor() { }
+  chatID: any = '';
+  chat: any;
+
+  constructor(
+    private firestore: AngularFirestore,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.getChatID();
+    
+  }
+
+  getChatID() {
+    this.route.paramMap.subscribe(paramMap => {
+      this.chatID = paramMap.get('ID');
+      this.loadPrivateChat();
+    });
+  }
+
+  loadPrivateChat() {
+    console.log('current chat id is ', this.chatID);
+    this.firestore
+      .collection('chats')
+      .doc(this.chatID)
+      .valueChanges()
+      .subscribe((chat: any) => {
+        this.chat = chat;
+      });
   }
 
 }
